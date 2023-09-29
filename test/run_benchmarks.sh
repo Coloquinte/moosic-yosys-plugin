066dfb960b03afa7cf9b6ddc5782db400b49df39
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2023-09-29 13:26:51 +0200

    Run more benchmarks and export the results as artifacts + restrict the run to ISCAS85

--- test/run_benchmarks.sh
+++ test/run_benchmarks.sh
@@ -1,12 +1,42 @@
 #!/bin/bash
 
-for benchmark in benchmarks/blif/*.blif
+dirs="logs scripts"
+targets="pairwise corruption"
+key_percents="1 2 5 10"
+
+cd benchmarks
+
+for dir in $dirs
+do
+	for target in $targets
+	do
+		for key_percent in $key_percents
+		do
+			mkdir -p "${dir}/${target}/key_${key_percent}pc"
+		done
+	done
+done
+
+for benchmark in blif/c*.blif
 do
         name=$(basename "${benchmark}" .blif)
 	echo "Running benchmark ${name}"
-        for target in pairwise corruption
+        for target in $targets
         do
-                yosys -m moosic-yosys-plugin  -p "read_blif ${benchmark}; flatten; synth; logic_locking -key-percent ${key_percent} -target ${target}" > /dev/null \
-                        || { echo "Failure on ${name} target ${target}"; exit 1; }
-        done
+		echo "    Target ${target}"
+		for key_percent in $key_percents
+		do
+			echo "        Key ${key_percent}%"
+			log_file="logs/${target}/key_${key_percent}pc/${name}.log"
+			script_file="scripts/${target}/key_${key_percent}pc/${name}.ys"
+			echo "read_blif ${benchmark}" > "${script_file}"
+			echo "flatten; synth" >> "${script_file}"
+			echo "logic_locking -key-percent ${key_percent} -target ${target}" >> "${script_file}"
+			cmd="yosys -m moosic-yosys-plugin -s ${script_file} > ${log_file}"
+			eval "$cmd" || { echo "Failure on ${name} target ${target} key ${key_percent}%: ${cmd}"; exit 1; }
+		done
+	done
 done
+
+cd ..
+tar -c benchmarks/scripts/ benchmarks/logs | xz -9 - > benchmark_results.tar.xz
