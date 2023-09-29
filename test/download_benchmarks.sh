d3378e298c891749c6a1138d437b9a4d4ed6d32b
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2023-09-29 12:04:45 +0200

    Use github to host the compressed ISCAS tests rather than hammering the original server

--- test/download_benchmarks.sh
+++ test/download_benchmarks.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
+# Obtain a compressed version of the benchmarks
 
-mkdir -p benchmarks/bench
-mkdir -p benchmarks/blif
-
-cd benchmarks
-
-cd bench
-for bench in iscas85 iscas89 iscas99
-do
-	wget https://pld.ttu.ee/~maksim/benchmarks/${bench}/bench -qN -l1 -nH -np -r --cut-dirs=4 --reject="*.html*" --reject=robots.txt
-	rm bench
-done
-cd ..
-
-cd blif
-for bench in $(ls ../bench)
-do
-	yosys-abc -c "read_bench ../bench/${bench}; write_blif ${bench%%.bench}.blif" > /dev/null
-	sed -i "s/new_//g" "${bench%%.bench}.blif"
-done
-rm abc.history
-cd ..
-
-cd ..
+wget -qN https://github.com/Coloquinte/moosic-yosys-plugin/releases/download/iscas_benchmarks/benchmarks.tar.xz
+tar -xf benchmarks.tar.xz
