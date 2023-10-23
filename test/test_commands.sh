3260835f06036c8b829dbbaf8584e82005610714
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2023-10-23 13:07:56 +0200

    Add tests for more command options

--- test/test_commands.sh
+++ test/test_commands.sh
@@ -11,11 +11,17 @@ yosys -m moosic -p "read_blif benchmarks/blif/c1355.blif; flatten; synth; logic_
 # Basic hybrid
 yosys -m moosic -p "read_blif benchmarks/blif/c1355.blif; flatten; synth; logic_locking -target hybrid"
 
+# Basic pairwise without deduplication
+yosys -m moosic -p "read_blif benchmarks/blif/c1355.blif; flatten; synth; logic_locking -target pairwise-no-dedup"
+
 # Set key percent and key
 yosys -m moosic -p "read_blif benchmarks/blif/c1355.blif; flatten; synth; logic_locking -key-percent 5 -key 0a239e"
 
 # Set key bits and key
-yosys -m moosic -p "read_blif benchmarks/blif/c1355.blif; flatten; synth; logic_locking -key-percent 5 -key 0a239e"
+yosys -m moosic -p "read_blif benchmarks/blif/c1355.blif; flatten; synth; logic_locking -key-bits 10 -key 0a239e"
+
+# Set number of test vectors and keys
+yosys -m moosic -p "read_blif benchmarks/blif/c1355.blif; flatten; synth; logic_locking -nb-test-vectors 99 -nb-analysis-keys 95 -nb-analysis-vectors 1079"
 
 # Explore
 yosys -m moosic -p "read_blif benchmarks/blif/c1355.blif; flatten; synth; logic_locking -explore -output-dir logs"
