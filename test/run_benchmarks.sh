f21287d8d379bc827ddfde4994598f39fb8a0144
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2023-11-15 15:13:49 +0100

    Update benchmarking script to report Pareto front

--- test/run_benchmarks.sh
+++ test/run_benchmarks.sh
@@ -1,41 +1,28 @@
 #!/bin/bash
 
-dirs="logs scripts"
-targets="pairwise corruption"
-key_percents="1 2 5 10"
+dirs="logs scripts results"
+time_limit=600
+iter_limit=1000000
 
 cd benchmarks
 
 for dir in $dirs
 do
-	for target in $targets
-	do
-		for key_percent in $key_percents
-		do
-			mkdir -p "${dir}/${target}/key_${key_percent}pc"
-		done
-	done
+	mkdir -p "${dir}"
 done
 
 for benchmark in blif/c*.blif
 do
         name=$(basename "${benchmark}" .blif)
 	echo "Running benchmark ${name}"
-        for target in $targets
-        do
-		echo "    Target ${target}"
-		for key_percent in $key_percents
-		do
-			echo "        Key ${key_percent}%"
-			log_file="logs/${target}/key_${key_percent}pc/${name}.log"
-			script_file="scripts/${target}/key_${key_percent}pc/${name}.ys"
-			echo "read_blif ${benchmark}" > "${script_file}"
-			echo "flatten; synth" >> "${script_file}"
-			echo "logic_locking -key-percent ${key_percent} -target ${target}" >> "${script_file}"
-			cmd="yosys -m moosic -s ${script_file} > ${log_file}"
-			eval "$cmd" || { echo "Failure on ${name} target ${target} key ${key_percent}%: ${cmd}"; exit 1; }
-		done
-	done
+	log_file="logs/${name}.log"
+	script_file="scripts/${name}.ys"
+	result_file="results/${name}.csv"
+	echo "read_blif ${benchmark}" > "${script_file}"
+	echo "flatten; synth" >> "${script_file}"
+	echo "ll_explore -area -delay -corruptibility-estimate -iter-limit ${iter_limit} -time-limit ${time_limit} -output ${result_file}" >> "${script_file}"
+	cmd="yosys -m moosic -s ${script_file} > ${log_file}"
+	eval "$cmd" || { echo "Failure on ${name}: ${cmd}"; exit 1; }
 done
 
 cd ..
