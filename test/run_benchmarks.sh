3f93c6e8d9462c54c96cbffa4fefb442dcb85969
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2023-11-17 18:28:09 +0100

    Prepare commands for other benchmarking cases

--- test/run_benchmarks.sh
+++ test/run_benchmarks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dirs="logs scripts results"
+dirs="logs scripts estimate full area delay area_approx delay_approx"
 time_limit=600
 iter_limit=1000000
 
@@ -17,13 +17,21 @@ do
 	echo "Running benchmark ${name}"
 	log_file="logs/${name}.log"
 	script_file="scripts/${name}.ys"
-	result_file="results/${name}.csv"
 	echo "read_blif ${benchmark}" > "${script_file}"
 	echo "flatten; synth" >> "${script_file}"
-	echo "ll_explore -area -delay -corruptibility -test-corruptibility -output-corruptibility -iter-limit ${iter_limit} -time-limit ${time_limit} -output ${result_file}" >> "${script_file}"
+	# Full Pareto front
+	echo "ll_explore -area -delay -corruptibility -test-corruptibility -output-corruptibility -iter-limit ${iter_limit} -time-limit ${time_limit} -output full/${name}.csv" >> "${script_file}"
+	# Comparison between estimates
+	# echo "ll_explore -area -corruptibility -compare-estimate -iter-limit ${iter_limit} -time-limit ${time_limit} -output estimate/${name}.csv" >> "${script_file}"
+	# Area/corruptibility
+	# echo "ll_explore -area -corruptibility -no-estimate -iter-limit ${iter_limit} -time-limit ${time_limit} -output area_approx/${name}.csv" >> "${script_file}"
+	# echo "ll_explore -area -corruptibility -iter-limit ${iter_limit} -time-limit ${time_limit} -output area/${name}.csv" >> "${script_file}"
+	# Delay/corruptibility
+	# echo "ll_explore -delay -corruptibility -no-estimate -iter-limit ${iter_limit} -time-limit ${time_limit} -output delay_approx/${name}.csv" >> "${script_file}"
+	# echo "ll_explore -delay -corruptibility -iter-limit ${iter_limit} -time-limit ${time_limit} -output delay/${name}.csv" >> "${script_file}"
 	cmd="yosys -m moosic -s ${script_file} > ${log_file}"
 	eval "$cmd" || { echo "Failure on ${name}: ${cmd}"; exit 1; }
 done
 
 cd ..
-tar -c benchmarks/scripts/ benchmarks/logs benchmarks/results/ | xz -9 - > benchmark_results.tar.xz
+tar -c benchmarks/estimate benchmarks/area benchmarks/delay benchmarks/full | xz -9 - > benchmark_results.tar.xz
