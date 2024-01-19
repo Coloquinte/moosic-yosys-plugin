67ff4c4a8afb9d8895841b00ad54c1bc57d064b6
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2024-01-19 17:11:33 +0000

    Use external repository for the benchmarks

--- test/download_benchmarks.sh
+++ test/download_benchmarks.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Obtain a compressed version of the benchmarks
 
-wget -qN https://github.com/Coloquinte/moosic-yosys-plugin/releases/download/iscas_benchmarks/benchmarks_2.tar.xz
-tar -xf benchmarks_2.tar.xz
+wget -qN https://github.com/Coloquinte/LogicBenchmarks/releases/download/v1.0.0/logic_benchmarks.zip
+mkdir benchmarks
+unzip logic_benchmarks.zip -d benchmarks
