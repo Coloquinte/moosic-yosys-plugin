05a842ed340abac5ee768b3c84c4204f086492fb
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2024-02-01 11:17:23 +0000

    Update benchmark versions (Blif syntax fixes for Yosys compatibility)

--- test/download_benchmarks.sh
+++ test/download_benchmarks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Obtain a compressed version of the benchmarks
 
-wget -qN https://github.com/Coloquinte/LogicBenchmarks/releases/download/v1.0.1/logic_benchmarks.zip
+wget -qN https://github.com/Coloquinte/LogicBenchmarks/releases/download/v1.0.2/logic_benchmarks.zip
 mkdir benchmarks
 unzip logic_benchmarks.zip -d benchmarks
