6193defef24caad6f6b9c4d28522e599e6b3dbd0
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2024-04-09 18:21:37 +0100

    New script to check that all benchmarks can be locked correctly

---
+++ test/run_all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+cd benchmarks
+mkdir -p run_all
+
+function run_benchmark () {
+	benchmark=$1
+	antisat=$2
+	yosys -m moosic -p "read_blif -sop ${benchmark}; flatten; synth; logic_locking -target outputs -nb-antisat 16 -antisat ${antisat}; synth; check -assert"
+}
+
+for benchmark in blif/iscas85*.blif blif/iscas89*.blif blif/iscas99*.blif blif/lgsynth91*.blif blif/epfl*.blif blif/mcnc*.blif
+do
+	for antisat in none antisat caslock sarlock skglock
+	do
+		name=$(basename "${benchmark}" .blif)
+		echo "Running benchmark ${name} with sat countermeasure ${antisat}"
+		run_benchmark "${benchmark}" "${antisat}" > "run_all/${name}_${antisat}.log"
+	done
+done
+wait
