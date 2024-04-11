41a5b87cc847a87d1128551286ef92e8adababd2
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2024-04-11 11:07:07 +0100

    Add parallelism to greedy script

--- test/run_sat_attack.sh
+++ test/run_sat_attack.sh
@@ -3,29 +3,34 @@
 cd benchmarks
 mkdir -p sat_attack
 
+benchmarks="blif/iscas85*.blif blif/iscas89*.blif blif/iscas99*.blif blif/lgsynth91*.blif blif/epfl*.blif blif/mcnc*.blif"
 key=$(printf "7%.0s" {1..500})
 time_limit=120
 batch=2
+thresholds="0 0.1"
+countermeasures="antisat caslock sarlock skglock+"
+nb_antisats="16 24 32"
 
 function run_benchmark () {
 	benchmark=$1
 	error_threshold=$2
 	nb_antisat=$3
 	antisat=$4
+	name=$(basename "${benchmark}" .blif)
 	stdbuf -oL yosys -m moosic -p "read_blif -sop ${benchmark}; flatten; synth; logic_locking -target outputs -nb-antisat ${nb_antisat} -antisat ${antisat} -key ${key}; synth; ll_sat_attack -key ${key} -time-limit ${time_limit} -error-threshold ${error_threshold}" > "sat_attack/${name}_${antisat}_${nb_antisat}_${error_threshold}.log"
 }
 
 i=0
-for benchmark in blif/iscas85*.blif blif/iscas89*.blif blif/iscas99*.blif blif/lgsynth91*.blif blif/epfl*.blif blif/mcnc*.blif
+for benchmark in $benchmarks
 do
-	for error_threshold in 0 0.1
+	for error_threshold in $thresholds
 	do
-		for antisat in antisat caslock sarlock skglock+
+		for antisat in $countermeasures
 		do
-			for nb_antisat in 16 24 32
+			for nb_antisat in $nb_antisats
 			do
 				((i=i%batch)); ((i++==0)) && wait
-        			name=$(basename "${benchmark}" .blif)
+				name=$(basename "${benchmark}" .blif)
 				echo "Running benchmark ${name} with sat countermeasure ${antisat} (${nb_antisat}), threshold ${error_threshold}"
 				run_benchmark "${benchmark}" "${error_threshold}" "${nb_antisat}" "${antisat}" &
 			done
